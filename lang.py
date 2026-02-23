742b4a3b76158cd015504456bef36febe3c81a0c
samuelecionfoli-cmd <samuele.cionfoli@itisff.it>
2026-02-23 11:58:17 +0000

    modifica lang.py

--- lang.py
+++ lang.py
@@ -1,13 +1,17 @@
-from dotenv import dotenv_value
+from dotenv import dotenv_values
 
-from langchain.chat_models import init_chat_models
+from langchain.chat_models import init_chat_model
 
 userdata = dotenv_values()
 
-llm = init_chat_models(
-    model=userdata("MODEL")
-    , base_url = userdata("BASE_URL")
-    , api_key = userdata("CEREBRAS")
-    , temperature = userdata("TEMPERATURE") 
+llm = init_chat_model(
+    model=userdata["MODEL"]
+    , base_url = userdata["BASE_URL"]
+    , api_key = userdata["CEREBRAS"]
+    , temperature = userdata["TEMPERATURE"]
 )
+
+risposta = llm.invoke("chi e anders heijsberg e perche è importante? Rispondi brevemente")
+print(risposta.content)
+
 
